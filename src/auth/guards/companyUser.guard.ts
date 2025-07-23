1f69bc7cb99fe8ad5bdc24710b6e8b6fe5e4dc98
Imran Ahmed <imran.ahk26@gmail.com>
2025-07-23 16:12:31 +0600

    :wrench: fixed type issues using a shared type

--- src/auth/guards/companyUser.guard.ts
+++ src/auth/guards/companyUser.guard.ts
@@ -1,13 +1,14 @@
 import { CanActivate, ExecutionContext, Injectable, ForbiddenException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { USER_ROLES } from 'src/common/constants/enums';
+import { AuthenticatedRequest } from 'src/common/types/authenticated-request.types';
 
 @Injectable()
 export class CompanyUserGuard implements CanActivate {
   constructor(private prisma: PrismaService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
+    const request = context.switchToHttp().getRequest<AuthenticatedRequest>();
     const user = request.user;
     const companyId = request.params.id;
 
